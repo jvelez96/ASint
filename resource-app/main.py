e9e59cc83f5e24d760022ae65895521b85956f74
jvelez96 <j.velez2210@gmail.com>
2019-12-02 17:03:33 -0800

    bla

--- resource-app/main.py
+++ resource-app/main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/rooms")
+def rooms():
+    return render_template("rooms.html")
 
 
 if __name__ == "__main__":
