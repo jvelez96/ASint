d560890bcf86f4ef2c94b255280bdeb0cc46ed82
jvelez96 <j.velez2210@gmail.com>
2019-12-13 16:25:00 -0800

    added a few more things

--- resource-app/forms.py
+++ resource-app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from flask_babel import _, lazy_gettext as _l
 
 class NewSecretariatForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
