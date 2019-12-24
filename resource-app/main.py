e808b489fd73dc6ccf85f1ea978f2cc85bc19e2c
9dot5 <yorsilva122@gmail.com>
2019-12-24 02:34:43 +0000

    gcloud

--- resource-app/main.py
+++ resource-app/main.py
@@ -158,7 +158,12 @@ def callback():
         db.session.add(u)
         db.session.commit()
     except Exception as e:
-        logger.warning('user already exists')
+        logger.warning('user %s already exists', username)
+        db.session.rollback()
+
+    u = db.session.query(User).filter(User.username == username).first()
+    u.tokenn = token
+    db.session.commit()
 
     resp = make_response(redirect(url_for('home')))
     resp.set_cookie('username', username, secure=True)  #accessible in javascript
